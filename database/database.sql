87602ae75e97dacd5bc9e5cb947e1436432c09f0
leuat <nicolaas.groeneboom@gmail.com>
2022-05-04 09:13:02 +0200

    update dbase

--- database/database.sql
+++ database/database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
 -- Host: localhost    Database: nbw
 -- ------------------------------------------------------
--- Server version	8.0.28-0ubuntu0.20.04.3
+-- Server version	8.0.29-0ubuntu0.20.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,56 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `process`
+--
+
+DROP TABLE IF EXISTS `process`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `process` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `processtype_id` int NOT NULL,
+  `project_id` int NOT NULL,
+  `progress` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `process`
+--
+
+LOCK TABLES `process` WRITE;
+/*!40000 ALTER TABLE `process` DISABLE KEYS */;
+INSERT INTO `process` VALUES (1,1,11,NULL),(2,2,11,NULL),(3,4,11,NULL);
+/*!40000 ALTER TABLE `process` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `processtype`
+--
+
+DROP TABLE IF EXISTS `processtype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `processtype` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `value` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `processtype`
+--
+
+LOCK TABLES `processtype` WRITE;
+/*!40000 ALTER TABLE `processtype` DISABLE KEYS */;
+INSERT INTO `processtype` VALUES (1,'nutil'),(2,'webalign'),(3,'ilastik'),(4,'imagej'),(5,'ingestion');
+/*!40000 ALTER TABLE `processtype` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `project`
 --
 
@@ -25,8 +75,9 @@ DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
+  `projecttype_id` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,8 +86,32 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
-INSERT INTO `project` VALUES (2,'p2'),(3,'halla');
+INSERT INTO `project` VALUES (11,'heisann',10),(12,'hoppsann',11);
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projecttype`
+--
+
+DROP TABLE IF EXISTS `projecttype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projecttype` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `value` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projecttype`
+--
+
+LOCK TABLES `projecttype` WRITE;
+/*!40000 ALTER TABLE `projecttype` DISABLE KEYS */;
+INSERT INTO `projecttype` VALUES (10,'quint'),(11,'quint-imagej');
+/*!40000 ALTER TABLE `projecttype` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -121,4 +196,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-26 12:13:21
+-- Dump completed on 2022-05-04  9:12:55
